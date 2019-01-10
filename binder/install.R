207d613bbaf2dad82814a7a49b76c0004b58541d
Sasha Mikheyev <sasha [at] homologo.us>
2019-01-11 08:21:01 +0900

    added binder folder

---
+++ binder/install.R
@@ -0,0 +1,7 @@
+### install regular packages
+
+install.packages("ggplot2") # for plotting
+
+### install GitHub packages (tag = commit, branch or release tag)
+install.packages("devtools")
+devtools::install_github('covaruber/sommer')
